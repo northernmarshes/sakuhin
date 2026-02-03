5b071412310047d82873e5e3595250250f9fe4be
northernmarshes <northernmarshes@gmail.com>
2026-02-04 00:22:25 +0100

    260204-bootstrap-wip

--- src/config/settings.py
+++ src/config/settings.py
@@ -17,6 +17,9 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
